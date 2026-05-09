383170e53aa66c37735c2863cb441d4de07ebba8
GFT\gouc <gonzalo.llatser@gft.com>
2026-05-09 18:12:49 +0200

    feat(application): implement create product use case

--- src/main/java/com/gft/products/application/service/CreateProductService.java
+++ src/main/java/com/gft/products/application/service/CreateProductService.java
@@ -1,14 +1,29 @@
 package com.gft.products.application.service;
 
+import com.gft.products.application.exceptions.InvalidProductException;
 import com.gft.products.application.port.in.CreateProductPort;
+import com.gft.products.application.port.out.ProductRepositoryPort;
 import com.gft.products.domain.model.Product;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CreateProductService implements CreateProductPort {
+
+  private final ProductRepositoryPort productRepositoryPort;
 
   @Override
   public Product createProduct(String name, String description) {
-    throw new UnsupportedOperationException("Not implemented yet");
+    if (name == null || name.isBlank()) {
+      throw new InvalidProductException();
+    }
+
+    Product product = Product.builder()
+        .name(name.trim())
+        .description(description)
+        .build();
+
+    return productRepositoryPort.save(product);
   }
 }
